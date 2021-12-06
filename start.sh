771fe0abfd6aaf90fcf44c7ac069fe352f2cd320
Alone2 <alain.sinzig@gmail.com>
2021-12-06 20:47:58 +0100

    mc 1.18

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-docker run -p 25565:25565 --name mc1 --rm -v $(pwd)/servertest:/mcserver --env javaVer8or11or16=11 mc
+docker run -p 0.0.0.0:60606:60606/udp -p 25565:25565 --name mc1 --rm -v $(pwd)/servertest:/mcserver --env javaVersion=17 --env jarname=spigot.jar -d mc
+#docker run --network host --name mc1 --rm -v $(pwd)/servertest:/mcserver --env javaVer8or11or16=16 --env jarname=spigot.jar -d mc 
